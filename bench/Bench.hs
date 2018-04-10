67f77747c277d739ea7e0cd2d20e7e0be153dc39
Edward George <edwardgeorge@gmail.com>
2018-04-10 21:35:26 +0100

    bench: include benching for pattern-matching version

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Criterion.Main
 
-import Text.Utf8Validator
+import qualified Text.Utf8Validator as V
+import qualified Text.Utf8Validator2 as V2
 
 main :: IO ()
-main = defaultMain [bench "1" $ whnf validateBS "Hello-µ@ßöäüàá-UTF-8!!"]
+main =
+  defaultMain
+    [ bench "1" $ whnf V.validateBS "Hello-µ@ßöäüàá-UTF-8!!"
+    , bench "2" $ whnf V2.validateBS "Hello-µ@ßöäüàá-UTF-8!!"
+    ]
